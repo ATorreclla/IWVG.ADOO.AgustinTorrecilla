146472043f5486bf28126f30f1afad722882f33d
ATorrecilla <a.torrecilla@alumnos.upm.es>
2018-09-30 07:49:50 +0200

    commit develop

--- src/main/java/es/upm/miw/iwvg/adoo/utils/Constants.java
+++ src/main/java/es/upm/miw/iwvg/adoo/utils/Constants.java
@@ -2,9 +2,13 @@ package es.upm.miw.iwvg.adoo.utils;
 
 public class Constants {
 
+    public static final int NUMBER_BALL_GUESS = 4;
+
     public static final int NUMBER_OF_PLAYERS = 2;
 
     public static final int NUMBER_OF_PLAYS = 10;
+
+    public static final int NUMBER_OF_PLAY_MODES = 2;
 
     public static final String DEAD = "%d muertos ";
 
@@ -14,8 +18,6 @@ public class Constants {
 
     public static final String TITTLE = "\nAnalisis y Diseño \nOrientacion a Objetos \nJuego MasterMind\n";
 
-    public static final int NUMBER_BALL_GUESS = 4;
-
     public static final String PLAY_HUMAN = "1. Partida ";
 
     public static final String PLAY_COMPUTER = "2. Demo ";
@@ -23,5 +25,21 @@ public class Constants {
     public static final String OPTION = "Opcion? ";
 
     public static final String NUMBER_MOVES_COMPLETES = "Numero de jugadas realizadas: ";
+
+    public static final String GOODBYE = "Fin del juego";
+
+    public static final String PATTERN_PLAY = "[ABNRVZ]{%d}";
+
+    public static final String SECRET_ANSWER = "El código de colores secretos es: [%s] ";
+
+    public static final String PATTERN_YES_NO = "[YN]{1}";
+
+    public static final String STRINGCHAIN = "de cadena de caracteres";
+
+    public static final String WRITEFORMATERROR = "Hay un error en la entrada de datos: ";
+
+    public static final String DAMAGE = "damage";
+
+    public static final String NEGATION = "N";
 
 }
--- src/main/java/es/upm/miw/iwvg/adoo/utils/IO.java
+++ src/main/java/es/upm/miw/iwvg/adoo/utils/IO.java
@@ -1,4 +1,22 @@
 package es.upm.miw.iwvg.adoo.utils;
 
 public class IO {
+
+
+
+    public void writeln() {
+        System.out.println();
+    }
+
+    public void write(String title) {
+        System.out.print(title);
+    }
+
+    public void writeln(String title) {
+        System.out.println(title);
+    }
+
+    private void writeError(String formato) {
+        System.out.println(Constants.WRITEFORMATERROR + formato );
+    }
 }
